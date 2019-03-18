1a5f64648725fe4ffe4c2102d39f2f8ac08bd976
piv <dmitry.pivovarchyk@gmail.com>
2019-03-18 15:31:50 +0300

    Project structure refactor

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Spravka1 from '../spravka1'
+
+import  './app.css'
+
+const App = () => {
+    return (
+        <div>
+            <Spravka1/>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +11,11 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
