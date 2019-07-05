ae7745c0797e0b2516b8285c1554ff9bbf482d67
BRNV\say <igor.say@gmail.com>
2019-07-05 08:44:44 +0300

    add vagons pix

--- src/components/pages/clients/modal-edit-client.js
+++ src/components/pages/clients/modal-edit-client.js
@@ -6,12 +6,12 @@ const ModalEditClient = ({show, item, handleClose,clientUpdate, handleUpdate}) =
             <div>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header  className="gruz-bg-1 " closeButton>
-                        <Modal.Title className='text-primary '>Клиент: {item.value}</Modal.Title>
+                        <Modal.Title className='text-primary '>Клиент: {item.kods}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body className="gruz-bg-2">
                         <div >
                             <span className={'pl-3 font-italic'}>Наименование</span>
-                            <Input name={'label'} value={item.label}  onChange={handleUpdate} placeholder={''} />
+                            <Input name={'name'} value={item.name}  onChange={handleUpdate} placeholder={''} />
                             <br />
                             <span className={'pl-3 font-italic'}>Адрес</span>
                             <Input name={'adress'}  value={item.adress ? item.adress : ''} onChange={handleUpdate}  placeholder={''} />
@@ -20,7 +20,7 @@ const ModalEditClient = ({show, item, handleClose,clientUpdate, handleUpdate}) =
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>Закрыть</Button>
-                        <Button variant="primary" onClick={()=>clientUpdate({kods: item.value, name: item.label, adress: item.adress})}>Сохранить</Button>
+                        <Button variant="primary" onClick={()=>clientUpdate(item)}>Сохранить</Button>
                     </Modal.Footer>
                 </Modal>
 
