7fc5455cf071d9b4f7d5f2e1c2b702436f06dea2
BRNV\say <igor.say@gmail.com>
2019-07-05 08:57:29 +0300

    add find poezd

---
+++ src/components/pages/findpoezd/find-poezd-vagons-table.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import BootstrapTable from 'react-bootstrap-table-next'
+import FindVagonsHistory from '../findvagonhistory/index'
+
+class FindPoezdVagonsTable extends Component {
+    render() {
+        const {data, columns,selectedRow, selectRow, closeExpanded}=this.props
+        const expandRow = {
+            renderer: () => (
+                <div className={'p-0 m-0'}>
+                    <FindVagonsHistory vagon={selectedRow} closeExpanded={closeExpanded}/>
+                </div>
+            ),
+            onlyOneExpanding: true,
+            expandByColumnOnly: true,
+            expanded: selectedRow=== null ? [] : [selectedRow.id]
+        };
+        const rowEvents = {
+            onClick: (e, row ) => {
+                selectRow({id: row.Id});
+
+            }
+        }
+        return (
+            <div >
+                <BootstrapTable keyField='Id' data={ data } classes={'gruz-bg-1 mt-1'} columns={ columns }  condensed expandRow={ expandRow } rowEvents={ rowEvents }  />
+            </div>
+        )
+    }
+}
+
+export default FindPoezdVagonsTable
---
+++ src/components/pages/findpoezd/find-poezd-vagons-ui.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import FindPoezdVagonsTable from './find-poezd-vagons-table'
+import {SpravkaHeader, SpravkaHeaderCaption} from '../../shared/headers/index'
+
+const FindPoezdVagonsUI = ({
+                               loading,
+                               closeExpanded,
+                               caption,
+                               data,
+                               columns,
+                               selectedRow,
+                               selectRow,
+                               closeExpandedVagon
+                           }) =>
+            <Row  className="p-1 m-1 gruz-bg-4">
+                <Col>
+                    <SpravkaHeader loading={loading}  closeExpanded={closeExpanded} />
+                    <Row className='pt-1'>
+                        <Col  className='p-0'>
+                            <SpravkaHeaderCaption caption={caption} />
+                            <FindPoezdVagonsTable data={data}
+                                                  columns={columns}
+                                                  selectedRow={selectedRow}
+                                                  selectRow={selectRow}
+                                                  closeExpanded={closeExpandedVagon}
+                            />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
+export default FindPoezdVagonsUI
